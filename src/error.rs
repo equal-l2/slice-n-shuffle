1a8677384ff2fe76534ccd9f0152bcd0def04fc4
equal-l2 <eng.equall2@gmail.com>
2023-10-29 05:45:25 +0900

    Support WASM build

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,16 @@
 #[derive(thiserror::Error, Debug)]
+#[cfg_attr(feature = "js", derive(serde_with::SerializeDisplay))]
 pub enum Error {
-    #[error("Failed to open image: {0}")]
+    #[error("Failed to open the image: {0}")]
     Open(std::io::Error),
-    #[error("Failed to get image dimensions: {0}")]
-    ReadDimensions(image::ImageError),
-    #[error("Dimensions ({width}, {height}) are not divisible by ({x_split}, {y_split})")]
+    #[error("The dimensions of the image ({width}, {height}) are not divisible by ({x_split}, {y_split})")]
     DimensionMismatch {
         width: u32,
         height: u32,
         x_split: u32,
         y_split: u32,
     },
-    #[error("Failed to initialize RNG due to failure of entropy retrieval: {0}")]
-    GetEntropy(getrandom::Error),
-    #[error("Failed to decode the image: {0}")]
+    #[error("Failed to open the image in decoding: {0}")]
     Decode(image::ImageError),
     #[error("Failed to convert the image: {0}")]
     Convert(image::ImageError),
@@ -22,3 +19,12 @@ pub enum Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(feature = "js")]
+use wasm_bindgen::prelude::*;
+#[cfg(feature = "js")]
+impl From<Error> for JsValue {
+    fn from(e: Error) -> Self {
+        serde_wasm_bindgen::to_value(&e).unwrap()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::{num::NonZeroU32, path::Path};
+use std::num::NonZeroU32;
+use std::path::Path;
 
 mod error;
 mod random;
@@ -62,3 +63,44 @@ pub fn decode_with_path(
 ) -> Result<()> {
     load_and_save_with(from, to, |img| decode_image(img, x_split, y_split))
 }
+
+#[cfg(feature = "js")]
+mod wasm_fn {
+    use std::num::NonZeroU32;
+
+    use wasm_bindgen::prelude::*;
+
+    fn convert_to_nonzero_u32(
+        x_split: u32,
+        y_split: u32,
+    ) -> Result<(NonZeroU32, NonZeroU32), JsValue> {
+        let x_split = NonZeroU32::new(x_split).ok_or("x_split must be a non-zero number")?;
+        let y_split = NonZeroU32::new(y_split).ok_or("x_split must be a non-zero number")?;
+        Ok((x_split, y_split))
+    }
+
+    fn decode_into_image(buf: &[u8]) -> Result<image::RgbaImage, super::Error> {
+        image::load_from_memory(buf)
+            .map_err(super::Error::Decode)
+            .map(|i| i.to_rgba8())
+    }
+
+    #[wasm_bindgen]
+    pub fn encode_image_buffer(buf: &[u8], x_split: u32, y_split: u32) -> Result<Vec<u8>, JsValue> {
+        let (x_split, y_split) = convert_to_nonzero_u32(x_split, y_split)?;
+        let img = decode_into_image(buf).map_err(Into::<JsValue>::into)?;
+        let img_out = super::encode_image(&img, x_split, y_split).map_err(Into::<JsValue>::into)?;
+        Ok(img_out.into_raw())
+    }
+
+    #[wasm_bindgen]
+    pub fn decode_image_buffer(buf: &[u8], x_split: u32, y_split: u32) -> Result<Vec<u8>, JsValue> {
+        let (x_split, y_split) = convert_to_nonzero_u32(x_split, y_split)?;
+        let img = decode_into_image(buf).map_err(Into::<JsValue>::into)?;
+        let img_out = super::decode_image(&img, x_split, y_split).map_err(Into::<JsValue>::into)?;
+        Ok(img_out.into_raw())
+    }
+}
+
+#[cfg(feature = "js")]
+pub use wasm_fn::*;
